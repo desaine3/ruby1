6d054ba246f2dc88b2554c3c25b7cef32c11aba4
Tom Close <tom.close@cantab.net>
2013-07-09 09:57:20 +0100

    Cleaned up hello_you test

--- test_hello_you.rb
+++ test_hello_you.rb
@@ -19,12 +19,12 @@ end
 # The actual test
 describe 'hello_you' do
 	it "works for a random name" do
-		ProcessPilot::pilot('hello_you.rb', :force_ruby_process_sync => true, :debug=> true) do |oStdin, iStdout|
+		ProcessPilot::pilot('hello_you.rb', :force_ruby_process_sync => true) do |oStdin, iStdout|
 		  	iStdout.readpartial(100) # => "Enter your name: "
 		  	oStdin.write("Boris the Newt\n")
 
 		  	output =  iStdout.gets.chomp # => "Hello Boris the newt"
-		  	assert_equal output, "Hello Boris the Newt"
+		  	assert_equal "Hello Boris the Newt", output
 
 		end
 
