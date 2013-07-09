a46989d4256c1f4f7a87324d6d975b4f46270776
Tom Close <tom.close@cantab.net>
2013-07-09 09:51:00 +0100

    Cleaned up divide cake test

--- test_divide_cake.rb
+++ test_divide_cake.rb
@@ -19,25 +19,25 @@ end
 # The actual test
 describe 'divide_cake' do
 	it "works for 11 and 3" do
-		ProcessPilot::pilot('divide_cake.rb', :force_ruby_process_sync => true, :debug=> true) do |oStdin, iStdout|
+		ProcessPilot::pilot('divide_cake.rb', :force_ruby_process_sync => true) do |oStdin, iStdout|
 		  	iStdout.readpartial(100) # => How many pieces of cake do you have? 11
 		  	oStdin.write("11\n")
 		  	iStdout.readpartial(100) # => How many people want some cake? 3
 		  	oStdin.write("3\n")
 		  	output =  iStdout.gets.chomp # => 
-		  	assert_equal output, "Give each person 3 pieces. There will be 2 additional pieces for you to eat as soon as they leave."
+		  	assert_equal "Give each person 3 pieces. There will be 2 additional pieces for you to eat as soon as they leave.", output
 
 		end
 
 	end
 	it "works for 4 and 5" do
-		ProcessPilot::pilot('divide_cake.rb', :force_ruby_process_sync => true, :debug=> true) do |oStdin, iStdout|
+		ProcessPilot::pilot('divide_cake.rb', :force_ruby_process_sync => true) do |oStdin, iStdout|
 		  	iStdout.readpartial(100) # => How many pieces of cake do you have? 11
 		  	oStdin.write("4\n")
 		  	iStdout.readpartial(100) # => How many people want some cake? 3
 		  	oStdin.write("5\n")
 		  	output =  iStdout.gets.chomp # => 
-		  	assert_equal output, "Give each person 0 pieces. There will be 4 additional pieces for you to eat as soon as they leave."
+		  	assert_equal "Give each person 0 pieces. There will be 4 additional pieces for you to eat as soon as they leave.", output
 
 		end
 
